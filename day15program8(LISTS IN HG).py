67ebab0b85ca940265b9e04784371bb2f736f4ef
Yuvaraja9573 <121186379+Yuvaraja9573@users.noreply.github.com>
2023-01-06 17:51:48 +0530

    Add files via upload

---
+++ day15program8(LISTS IN HG).py
@@ -0,0 +1,26 @@
+
+
+if __name__ == '__main__':
+    N = int(input())   
+    arr=list()
+    for i in range(N):
+        inp=input().strip().split()
+        if inp[0]=='insert':
+            i=int(inp[1])
+            e=int(inp[2])
+            arr.insert(i,e)
+        if inp[0]=='print':
+            print(arr)
+        if inp[0]=='remove':
+            e=int(inp[1])
+            if e in arr:
+                arr.remove(e)
+        if inp[0]=='append':
+            e=int(inp[1])
+            arr.append(e)
+        if inp[0]=='sort':
+            arr.sort()
+        if inp[0]=='pop':
+            arr.pop()
+        if inp[0]=='reverse':
+            arr.reverse()
